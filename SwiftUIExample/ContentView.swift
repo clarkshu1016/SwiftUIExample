9e95e594254bcf61d54608a47ee87d906fd99dd2
clarkshu <sxchao2008@gmail.com>
2022-01-02 13:24:52 +0900

    First commit

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -14,8 +14,14 @@ struct ContentView: View {
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func sayGoodMorning(){
+    print("Good monrning")
 }
